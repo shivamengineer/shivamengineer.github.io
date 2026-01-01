7ddba56dd4c2548fec5c34f63ecd7a988a8c3433
Shivam <shivam.engineer1@gmail.com>
2026-01-01 14:01:57 -0500

    changed queue to horizontal

--- BasicDataStructures/Queue/Queue.js
+++ BasicDataStructures/Queue/Queue.js
@@ -7,24 +7,24 @@ class Queue {
 
     setElementTargetPosition(){
         for(var i = 0; i < this.elements.length; i++){
-            var difference = innerHeight * 0.05 * i;
-            this.elements[i].setTargetDrawingAttributes(innerWidth * 0.4, innerHeight * 0.1 + difference);
+            var difference = innerHeight * 0.08 * i;
+            this.elements[i].setTargetDrawingAttributes(innerWidth * 0.1 + difference, innerHeight * 0.3);
         }
     }
 
     enqueue(element) {
         var e = new Element(element);
-        var difference = innerHeight * 0.05 * this.items.length;
-        e.setElementDrawingAttributes(innerWidth * 0.1, innerHeight * 0.1, innerWidth * 0.1, innerHeight * 0.05);
-        e.setTargetDrawingAttributes(innerWidth * 0.4, innerHeight * 0.1 + difference);
+        var difference = innerHeight * 0.08 * this.items.length;
+        e.setElementDrawingAttributes(innerWidth * 0.1, innerHeight * 0.1, innerWidth * 0.04, innerHeight * 0.05);
+        e.setTargetDrawingAttributes(innerWidth * 0.1 + difference, innerHeight * 0.3);
         this.items.push(element);
         this.elements.push(e);
     }
 
     dequeue() {
         if (this.items.length > 0){
-            var difference = innerHeight * 0.05 * this.leaving.length;
-            this.elements[0].setTargetDrawingAttributes(innerWidth * 0.8, innerHeight * 0.2 + difference);
+            var difference = innerHeight * 0.06 * this.leaving.length;
+            this.elements[0].setTargetDrawingAttributes(innerWidth * 0.6 + difference, innerHeight * 0.1);
             this.leaving.push(this.elements.shift());
             this.setElementTargetPosition();
             return this.items.shift();
@@ -64,11 +64,6 @@ class Queue {
             ctx.strokeStyle = "white";
             ctx.fillStyle = "white";
             ctx.font = "20px Arial";
-            if(this.elements.length > 0 && this.elements[0].moving){
-                ctx.moveTo(innerWidth * 0.4, innerHeight * 0.05);
-                ctx.lineTo(innerWidth * 0.6, innerHeight * 0.05);
-                ctx.stroke();
-            }
             for(var i = 0; i < this.elements.length; i++){
                 this.elements[i].drawElement();
                 this.elements[i].updateDrawingAttributes();
