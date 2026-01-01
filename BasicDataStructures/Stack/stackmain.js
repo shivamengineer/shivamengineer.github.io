1292a96230af2f86b630dccea5306d47675b7bb8
Shivam <shivam.engineer1@gmail.com>
2026-01-01 01:19:58 -0500

    added stack and animations

--- BasicDataStructures/Stack/stackmain.js
+++ BasicDataStructures/Stack/stackmain.js
@@ -4,6 +4,23 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+var stack = new Stack();
+
+function pushNumber(){
+    const pushInput = document.getElementById("Push").value;
+    var num = parseInt(pushInput);
+    if(num != NaN && num >= 0){
+        stack.push(num);
+    }
+    document.getElementById("Push").value = "";
+}
+
+function popNumber(){
+    if(stack.size() > 0){
+        stack.pop();
+    }
+}
+
 function clearscreen(){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -11,6 +28,26 @@ function clearscreen(){
 
 function draw(){
     clearscreen();
+    stack.drawStack();
 }
 
+function keyboard(e){
+    switch(e.keyCode){
+        case 13:
+            const pushInput = document.getElementById("Push");
+            if(document.activeElement === pushInput){
+                this.pushNumber();
+            }
+            break;
+        case 46:
+            if(lastSelected != -999){
+                bst.deleteNode(bst.root, lastSelected);
+                bst.clearSelected(bst.root);
+            }
+            lastSelected = -999;
+            break;
+    }
+}
+
+window.addEventListener('keydown', keyboard);
 setInterval(draw, 16);
